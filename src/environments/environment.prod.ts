c6adff20c03f94ef8ad3ec5095ab41cc0ffeb58d
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2022-11-21 19:09:13 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  botOwnerId: '218459216145285121',
-  discordInvite: 'https://discord.gg/24tycau',
+  botOwnerId: '832932513936441375',
+  discordInvite: 'https://discord.gg/X4H7C8fcJm',
   endpoint: '/api',
-  githubURL: 'https://github.com/theADAMJR/2PG-Dashboard',
+  githubURL: 'https://github.com/ShadowGamer1221',
   production: true,
   docsURL: 'https://docs.2pg.xyz',
   stripePublicKey: 'pk_live_Y4fYfqvEgZNsllNFo4TeFcmv005D0tNBWQ',
